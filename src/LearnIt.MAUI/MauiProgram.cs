20300f7d7a3715c5a44e8b0d3e16e977aac6558a
fordrelak <fordrelak.4050@yandex.ru>
2022-05-17 21:27:29 +0300

    add fonts and finish first page

---
+++ src/LearnIt.MAUI/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿namespace LearnIt.MAUI
+{
+    public static class MauiProgram
+    {
+        public static string BaseAddress =
+            DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5114" : "http://localhost:5114";
+
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("Montserrat-Italic-VariableFont_wght.ttf", "Montserrat");
+                    fonts.AddFont("Montserrat-VariableFont_wght.ttf", "Montserrat");
+                });
+
+            ConfigureServices(builder);
+
+            return builder.Build();
+        }
+
+        private static void ConfigureServices(MauiAppBuilder builder)
+        {
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+        }
+    }
+}
---
+++ src/LearnIt.MAUI/Services/LocalStorageService.cs
@@ -0,0 +1,21 @@
+﻿namespace LearnIt.MAUI.Services
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>")]
+    public class LocalStorageService
+    {
+        public void Add(string key, string value)
+        {
+            Preferences.Set(key, value);
+        }
+
+        public void Get(string key, string defaultValue = "")
+        {
+            Preferences.Get(key, defaultValue);
+        }
+
+        public void Remove(string key)
+        {
+            Preferences.Remove(key);
+        }
+    }
+}
